dbb69558cb0dd5a6d6d3bb688ab4a285fe95c78b
snapdragon teaks <inutilidades639@gmail.com>
2023-09-19 21:30:13 -0300

    add AnyKernel3 zip script to Build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,7 +8,18 @@ mkdir out
 
 make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android VARIANT_DEFCONFIG=sdm450_sec_j8y18lte_swa_open_defconfig sdm450_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig 
 
-make -j64 -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
+make -j12 -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+IMAGE="out/arch/arm64/boot/Image.gz"
+DTB_OUT="out/arch/arm64/boot/Image.gz-dtb"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+
+if [[ -f "$IMAGE" ]]; then
+	rm AnyKernel3/*.zip > /dev/null 2>&1
+	cp $IMAGE AnyKernel3/Image.gz
+	cd AnyKernel3
+	zip -r9 Kernel-J8-beta.zip .
+fi
 
