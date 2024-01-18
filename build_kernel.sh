5e64aa97ab8f0ad07161b47222174b90a13e9ae8
snapdragon teaks <inutilidades639@gmail.com>
2024-01-18 08:58:18 -0300

    build script : copy only kernel with dtb integrated

--- build_kernel.sh
+++ build_kernel.sh
@@ -10,16 +10,14 @@ make -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFL
 
 make -j12 -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
 
-IMAGE="out/arch/arm64/boot/Image.gz"
-DTB_OUT="out/arch/arm64/boot/Image.gz-dtb"
+IMAGE="out/arch/arm64/boot/Image.gz-dtb"
 
 DATE_END=$(date +"%s")
 DIFF=$(($DATE_END - $DATE_START))
 
 if [[ -f "$IMAGE" ]]; then
 	rm AnyKernel3/*.zip > /dev/null 2>&1
-	cp $IMAGE AnyKernel3/Image.gz
-	cp $DTB_OUT AnyKernel3/Image.gz-dtb
+	cp $IMAGE AnyKernel3/Image.gz-dtb
 	cd AnyKernel3
 	zip -r9 Kernel-J8-beta.zip .
 fi
